82dbd57d98c554c3383e0068224be99184f57611
bridget462 <bridget.dev.create@gmail.com>
2020-08-22 20:12:16 -0700

    tweet and new quote btn function implemented

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 // Get Quote From API
 async function getQuote() {
   // some open api cannot be connected from local host. to avoid this, call proxy -> api
@@ -8,12 +14,35 @@ async function getQuote() {
     // wait until you get api respone
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+    // if author is blank, add Unknown
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // reduce font size for longer quote
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
   } catch (error) {
     getQuote();
-    console.log("whoops, no quote", error);
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listners
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuote();
